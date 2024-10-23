48cecaf9608b0e0607476bda70477055f01f5bec
ella-an2 <ella.an@mths.ca>
2024-10-23 10:37:22 -0400

    changed some things

--- main.ts
+++ main.ts
@@ -12,18 +12,18 @@ const servoNumber1 = robotbit.Servos.S1
 basic.showIcon(IconNames.Happy)
 
 input.onButtonPressed(Button.A, function () {
-    robotbit.Servo(servoNumber1, 0)
+    robotbit.Servo(servoNumber1, 5)
     basic.clearScreen()
-    basic.showString('0')
+    basic.showString('5')
     basic.showIcon(IconNames.SmallSquare)
     basic.clearScreen()
     basic.showIcon(IconNames.Happy)
 })
 
 input.onButtonPressed(Button.B, function () {
-    robotbit.Servo(servoNumber1, 180)
+    robotbit.Servo(servoNumber1, 75)
     basic.clearScreen()
-    basic.showString('180')
+    basic.showString('75')
     basic.showIcon(IconNames.SmallSquare)
     basic.clearScreen()
     basic.showIcon(IconNames.Happy)
